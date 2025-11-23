f657e9f0723661a07525d1457395a07d998eaf79
Xiuyuan Yu <xiuyuanyu86@gmail.com>
2025-11-22 18:49:56 -0600

    Solve hydration issue.

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,7 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-// Use hydrateRoot because the HTML will be pre-rendered
-ReactDOM.hydrateRoot(
-  rootElement,
+const app = (
   <React.StrictMode>
     {/* basename ensures routing works if deployed to a subdirectory */}
     <BrowserRouter basename={import.meta.env.BASE_URL}>
@@ -18,3 +16,11 @@ ReactDOM.hydrateRoot(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+// In development (vite dev), the root is empty, so we use createRoot (SPA mode).
+// In production (pre-rendered), the root has content, so we use hydrateRoot (SSG mode).
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrateRoot(rootElement, app);
+} else {
+  ReactDOM.createRoot(rootElement).render(app);
+}
